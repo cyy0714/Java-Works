8436b49c8e0f02b98aaa751861bb12d44ab343e4
Yung-Yu Chung <acekoko@gmail.com>
2015-11-18 14:43:18 -0600

    Move test files to Test package

---
+++ JavaWorkspace/LeetCode/src/LeetCodes/Test/StringInterleaving_Test.java
@@ -0,0 +1,13 @@
+package LeetCodes.Test;
+
+import LeetCodes.String.Solution;
+
+public class StringInterleaving_Test {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.isInterleave("aa", "ab", "aaba"));
+
+	}
+
+}
